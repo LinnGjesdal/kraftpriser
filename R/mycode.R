cf135da4954fe22f22f4a17c97f9bb287084af22
LinnGjesdal <linn.gjesdal@pwc.com>
2019-10-26 18:05:20 +0200

    hente data fra api

---
+++ R/mycode.R
@@ -0,0 +1,23 @@
+# First function goes here
+
+# use Ctrl + Shift + L to avoid storing the package in the global environment
+
+# The function within the package must be documented using roxygen: Ctrl + Shift + Alt + R
+
+#' use_api
+#'
+#' @description use_api loads data from about Norwegian hydro power from Norges vassdrags- og energidirektorat (NVE) and saves it to a data frame.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+use_api <- function() {
+  pacman::p_load(httr, jsonlite, dplyr)
+  my_url <- paste0("https://www.nve.no/umbraco/api/Powerplant/GetHydroPowerPlantsInOperation")
+  my_raw_result <- httr::GET(my_url)
+  my_content <- httr::content(my_raw_result, as = 'text')
+  my_content_from_json <- jsonlite::fromJSON(my_content)
+
+}
+
