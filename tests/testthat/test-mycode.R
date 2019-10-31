1963ad5bc6f8113b17bfce89185a1d723c99f457
LinnGjesdal <linn.gjesdal@pwc.com>
2019-10-31 14:14:20 +0100

    Tests are written

--- tests/testthat/test-mycode.R
+++ tests/testthat/test-mycode.R
@@ -2,18 +2,27 @@
 
 # We need to test the function that loads the data from the api
 
-context("test the data load from the api")
+context("test the data load from the api and onto the data frame")
 
-test_that("the data frame shows as a list", {
-  df <- my_content_from_json
-  expect_type(df, list)
+
+test_that('the number of columns in the data frame is correct', {
+  data <- powerproduction()
+  expect_equal(ncol(data), 2)
 })
 
-test_that("data has been loaded from the api", {
-  f <- tempfile()
-  expect_true(file.exists(f))
+test_that('data types are correct', {
+  data <- powerproduction()
+  expect_is(data,'data.frame')
+  expect_is(data$Navn, 'character')
+  expect_is(data$MidProd_81_10, 'numeric')
 })
 
-test_that("test that the data from the api has been loaded as a data frame", {
-  expect_is(my_content_from_json, "data.frame")
+test_that('powerproduction_for_one works in the same way as powerproduction, but for a smaller data frame', {
+  df <- powerproduction_for_one(name_power_station = "Grøndal")
+  expect_equal(ncol(df), 2)
+  expect_equal(nrow(df), 1)
+  # the above tests that we have a smaller set, as we should only have data for one station
+  expect_is(df, 'data.frame')
+  expect_is(df$Navn, 'character')
+  expect_is(df$MidProd_81_10, 'numeric')
 })
